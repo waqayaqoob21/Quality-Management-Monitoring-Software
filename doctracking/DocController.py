557d5ffab0191411ce16b7e6dd52fcd2b43bf06c
waqaryaqoob <108149956+waqaryaqoob@users.noreply.github.com>
2022-08-02 17:09:51 +0500

    update to DocController.py
    updated

--- doctracking/DocController.py
+++ doctracking/DocController.py
@@ -133,8 +133,6 @@ class DocController:
 
     @staticmethod
     def GetDocumentPDFList(request):
-        wrapper = textwrap.TextWrapper(width=10)
-
         pdf = FPDF('L', 'mm', 'Legal')
         pdf.add_page()
         pdf.set_font('courier', 'B', 16)
@@ -142,23 +140,22 @@ class DocController:
         pdf.cell(40, 10, '',0,1)
         pdf.set_font("Times", size=10)
         line_height = pdf.font_size * 2.5
-        col_width = pdf.epw / 16
-        TABLE_COL_NAMES = ("Serial Number","Documnet Name", "Document Type", "Sender", "Receive Date", "Marked To", "Marked Date", "Due Date", "Task Date", "Status", "Sent To", "Sent Date", "Is Active","Base_64 File", "Attachment", "Remarks")
+        col_width = pdf.epw / 13
+        TABLE_COL_NAMES = ("Documnet Name", "Document Type", "Sender", "Receive Date", "Marked To", "Marked Date", "Due Date", "Task Date", "Status", "Sent To", "Sent Date", "Active","Remarks")
         def render_table_header():
-            pdf.set_font(style="B")  # enabling bold text
+            pdf.set_font(style="B") 
             for col_name in TABLE_COL_NAMES:
-                pdf.multi_cell(col_width, line_height, wrapper.fill(col_name), border=1, ln=3)
+                pdf.multi_cell(col_width, line_height, col_name, border=1, ln=3)
             pdf.ln(line_height)
-            pdf.set_font(style="")  # disabling bold text
-       
+            pdf.set_font(style="")
         render_table_header()
 
-        table_data = doctracking.objects.filter(due_date__lt=datetime.today()).values()
-        for row in table_data:
+        table_data = doctracking.objects.values('doc_name','doc_type','sender','receive_date','marked_to','marked_date','due_date','task_date','status','sent_to','sent_date','isActive','remarks')
+        for row_data in table_data:
             if pdf.will_page_break(line_height):
                 render_table_header()
-            for data in row:
-                pdf.multi_cell(col_width, line_height, data, border=1, ln=3)
+            for key, data  in  row_data.items():
+                pdf.multi_cell(col_width, line_height, f"{data}", border=1, ln=3)
             pdf.ln(line_height)
         pdf.output("report.pdf")
         return FileResponse(open('report.pdf', 'rb'), as_attachment=True, content_type='application/pdf')
