e08c85564abe625150326cb13ff829f7109f9d5c
waqaryaqoob <108149956+waqaryaqoob@users.noreply.github.com>
2022-08-03 17:30:36 +0500

    Update DocController.py
    few small changes

--- doctracking/DocController.py
+++ doctracking/DocController.py
@@ -183,20 +183,20 @@ class DocController:
     def GetDocumentExcelList(request):
         response = HttpResponse(content_type = 'application/ms-excel')
         response['Content-Disposition'] = 'attachment; filename="DocumentExcelSheet.xls"'
-        wb = xlwt.Workbook(encoding='utf-8')
-        ws = wb.add_sheet('ExcelSheet')
+        work_book = xlwt.Workbook(encoding='utf-8')
+        work_sheet = work_book.add_sheet('ExcelSheet')
         row_num = 0
         font_style = xlwt.XFStyle()
         font_style.font.bold = True
         TABLE_COL_NAMES = ("Document Name", "Document Type", "Sender", "Receive Date", "Marked To", "Marked Date", "Due Date", "Task Date", "Status", "Sent To", "Sent Date","Remarks")
         for col_num in range(len(TABLE_COL_NAMES)):
-            ws.write(row_num,col_num,TABLE_COL_NAMES[col_num], font_style)
+            work_sheet.write(row_num,col_num,TABLE_COL_NAMES[col_num], font_style)
         font_style = xlwt.XFStyle()
 
         data = doctracking.objects.filter(due_date__lt=datetime.today()).values_list('doc_name','doc_type','sender','receive_date','marked_to','marked_date','due_date','task_date','status','sent_to','sent_date','remarks')
         for row in data:
             row_num += 1
             for col_num in range(len(row)):
-                ws.write(row_num,col_num,str(row[col_num]), font_style)
-        wb.save(response)
+                work_sheet.write(row_num,col_num,str(row[col_num]), font_style)
+        work_book.save(response)
         return response
