13d11c94592b81c48821dc8ca873c0eb541830a3
waqaryaqoob01 <134327303+waqaryaqoob01@users.noreply.github.com>
2024-09-09 01:53:11 +0500

    Update QmsController.py

--- qms/QmsController.py
+++ qms/QmsController.py
@@ -322,10 +322,9 @@ class QmsController:
             dataList = QmsAudit.objects.filter(filter_objects)
 
             if current_status == 'Overdue Conducted':
-                dataList = QmsAudit.objects.filter(Q(audit_status="Conducted") | Q(audit_status="Completed"),audit_start_date__gt=F('planned_date'),
-                                                                    planned_date__year=current_year).annotate(
-                planned_date__month=Extract('planned_date', 'month'),planned_date__year=Extract('planned_date', 'year'),
-                planned_date__day=Extract('planned_date', 'day')).order_by('planned_date__month', 'planned_date__day','-planned_date__year')
+                dataList = QmsAudit.objects.filter(
+                    Q(audit_status="Conducted") | Q(audit_status="Completed"), audit_start_date__gt=F('planned_date'),
+                    planned_date__year=current_year)
 
             if current_status == 'QMS Total Under Process':
                 dataList = QmsAudit.objects.filter(audit_status = 'In-Process').annotate(
